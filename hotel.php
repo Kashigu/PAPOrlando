f5d965768eea494dec471e130e06d2d03a383dab
Orlando Lopes <78795585+Kashigu@users.noreply.github.com>
2021-03-04 12:39:29 +0000

    novos hoteis restaurantes

--- hotel.php
+++ hotel.php
@@ -5,7 +5,7 @@ toposingle();
 ?>
 
 <!--                                                 ESTÁ COM PROBLEMAS ESTA PÁGINA                                                      -->
-<div class="hero-listing set-bg" data-setbg="img/ratoeira1.jpg">
+<div class="hero-listing set-bg" data-setbg="img/categories/hoteis.jpg">
 </div>
 <!-- Hero Section End -->
 
@@ -18,7 +18,7 @@ toposingle();
                     <div class="about-intro">
                         <div class="rating">4.9</div>
                         <div class="intro-text">
-                            <h2>Ratoeira</h2>
+                            <h2>Templo</h2>
                             <h4>Marinha Grande</h4>
                             <p>Encontre alguns dos melhores sitios do Mundo</p>
                             <div class="open">Abre amanhã às 10 da manhã</div>
@@ -49,7 +49,7 @@ toposingle();
                 <div class="col-lg-8">
                     <div class="about-left">
                         <div class="about-desc">
-                            <h4>Sobre o Café</h4>
+                            <h4>Sobre o Hotel</h4>
                             <p>Donec eget efficitur ex. Donec eget dolor vitae eros feugiat tristique id vitae massa.
                                 Proin vulputate congue rutrum. Fusce lobortis a enim eget tempus. Class aptent taciti
                                 sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse
@@ -68,8 +68,50 @@ toposingle();
                                 vehicula mauris, eget eleifend tortor magna luctus</p>
                         </div>
                         <div class="about-video">
-                            <img class="tamanho" src="img/categories/hoteis.jpg" alt="">
-                            <a href="img/categories/hoteis.jpg" class="img-hover pop-up"><img src="img/zoom.png" alt=""></a>
+                            <div id="carouselExampleIndicators" class="tamanho carousel slide"
+                                 data-ride="carousel">
+                                <ol class=" carousel-indicators">
+                                    <li data-target="#carouselExampleIndicators"
+                                        data-slide-to="1"></li>
+                                    <li data-target="#carouselExampleIndicators"
+                                        data-slide-to="2"></li>
+                                    <li data-target="#carouselExampleIndicators"
+                                        data-slide-to="3"></li>
+                                    <li data-target="#carouselExampleIndicators"
+                                        data-slide-to="4"></li>
+                                </ol>
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <img class=" tamanho d-block w-100" src="img/hero-bg.jpg">
+                                        <a href="img/hero-bg.jpg" class="img-hover pop-up"><img src="img/zoom.png"
+                                                                                                alt="4"></a>
+
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img class=" tamanho d-block w-100" src="img/card.jpg">
+                                        <a href="img/card.jpg" class="img-hover pop-up"><img src="img/zoom.png" alt="3"></a>
+                                    </div>
+
+
+                                    <div class="carousel-item">
+                                        <img class=" tamanho d-block w-100" src="img/lisboa.jpg">
+                                        <a href="img/lisboa.jpg" class="img-hover pop-up"><img src="img/zoom.png" alt="2"></a>
+                                    </div>
+
+
+                                    <div class="carousel-item">
+                                        <img class=" tamanho d-block w-100" src="img/cant.jpg">
+                                        <a href="img/cant.jpg" class="img-hover pop-up"><img src="img/zoom.png" alt="1"></a>
+                                    </div>
+
+
+                                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button"
+                                       data-slide="prev">
+                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                        <span class="sr-only">Previous</span>
+                                    </a>
+                                </div>
+                            </div>
                         </div>
                         <div class="client-reviews">
                             <h3>Revisão</h3>
@@ -82,9 +124,12 @@ toposingle();
                                     <i class="fa fa-star"></i>
                                 </div>
                                 <h5>?A Great Place?</h5>
-                                <p>Donec eget efficitur ex. Donec eget dolor vitae eros feugiat tristique id vitae
-                                    massa. Proin vulputate congue rutrum. Fusce lobortis a enim eget tempus. Class
-                                    aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+                                <p>Donec eget efficitur ex. Donec eget dolor vitae eros feugiat
+                                    tristique id vitae
+                                    massa. Proin vulputate congue rutrum. Fusce lobortis a enim eget
+                                    tempus. Class
+                                    aptent taciti sociosqu ad litora torquent per conubia nostra, per
+                                    inceptos
                                     himenaeos. </p>
                                 <div class="client-text">
                                     <h5>Michael Smith</h5>
@@ -100,9 +145,12 @@ toposingle();
                                     <i class="fa fa-star"></i>
                                 </div>
                                 <h5>?The best food in town?</h5>
-                                <p>Donec eget efficitur ex. Donec eget dolor vitae eros feugiat tristique id vitae
-                                    massa. Proin vulputate congue rutrum. Fusce lobortis a enim eget tempus. Class
-                                    aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+                                <p>Donec eget efficitur ex. Donec eget dolor vitae eros feugiat
+                                    tristique id vitae
+                                    massa. Proin vulputate congue rutrum. Fusce lobortis a enim eget
+                                    tempus. Class
+                                    aptent taciti sociosqu ad litora torquent per conubia nostra, per
+                                    inceptos
                                     himenaeos. </p>
                                 <div class="client-text">
                                     <h5>Michael Smith</h5>
@@ -117,8 +165,8 @@ toposingle();
                         <div class="contact-info">
                             <div class="map">
                                 <iframe
-                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26440.72384129847!2d-118.24906619231132!3d34.06719475913053!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c659f50c318d%3A0xe2ffb80a9d3820ae!2sChinatown%2C%20Los%20Angeles%2C%20CA%2C%20USA!5e0!3m2!1sen!2sbd!4v1570213740685!5m2!1sen!2sbd"
-                                    height="385" style="border:0;" allowfullscreen="">
+                                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26440.72384129847!2d-118.24906619231132!3d34.06719475913053!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c659f50c318d%3A0xe2ffb80a9d3820ae!2sChinatown%2C%20Los%20Angeles%2C%20CA%2C%20USA!5e0!3m2!1sen!2sbd!4v1570213740685!5m2!1sen!2sbd"
+                                        height="385" style="border:0;" allowfullscreen="">
                                 </iframe>
                                 <img src="img/pin.png" alt="">
                             </div>
@@ -149,8 +197,6 @@ toposingle();
         </div>
     </div>
 </section>
-
-
 
 
 <?php
