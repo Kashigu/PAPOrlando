32b7fad9b589516678192f896eb484a0c30406cc
Orlando Lopes <78795585+Kashigu@users.noreply.github.com>
2021-03-05 11:29:38 +0000

    tentativa de meter filltableDistritos no distritos.php

--- distritos.php
+++ distritos.php
@@ -4,6 +4,34 @@ toponovo();
 
 ?>
 
+
+<script>
+    function confirmaElimina(id) {
+        $.ajax({
+            url: "AJAX/AJAXGetNameDistritos.php",
+            type: "post",
+            data: {
+                idDistrito: id
+            },
+            success: function (result) {
+                $('#contentModalDelete').html('Confirma que deseja eliminar o Distrito: ' + result + '?');
+                $('#idDistrito').val(id);
+                $('#staticBackdropDele').modal('toggle');
+                //if(confirm('Confirma que deseja eliminar a empresa:'+result+"?"))
+                // window.location="../elimina/eliminaEmpresa.php?id=" + id;
+            }
+        })
+    };
+
+
+    $('document').ready(function () {
+        $('#search').keyup(function () {
+            fillTableDistritos(this.value);
+        });
+        fillTableDistritos();
+    })
+</script>
+
 <section class="contact-section search-filter spad">
     <div class="container">
         <div class="row">
@@ -13,77 +41,16 @@ toponovo();
                         <h5>Nome dos Distritos</h5>
                         <input class="location-search1 nice-select1" type="text" id="search">
                     </div>
+
                     <button type="submit">Voltar</button>
                 </form>
             </div>
+            <div id="tableContent">
+            </div>
         </div>
-        <div class="col-12 mt-4">
-            <table class="table table-striped table-hover">
-                <td colspan="5" align='right'>
-                    <a data-toggle="modal" data-target="#distrito"><i class='fas text-dark'> Adiciona</i></a>
-                </td>
-                </tr>
-                <tr>
-                    <th width="3%">Id</th>
-                    <th width="3%">Nome</th>
-                    <th colspan="3" class="centertext">Opções</th>
-                </tr>
-                <tr>
-                    <td>1</td>
-                    <td>Leiria</td>
-                    <td width="5%" class="centertext">Editar</td>
-                    <td width="5%" class="centertext">Eliminar</td>
-                </tr>
-            </table>
-        </div>
-
-        <div class="col-12 mt-4">
-            <table class="table table-striped table-hover">
-                <tr>
-                    <th width="3%">Id</th>
-                    <th width="3%">Nome</th>
-                    <th colspan="3" class="centertext">Opções</th>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Porto</td>
-                    <td width="5%" class="centertext">Editar</td>
-                    <td width="5%" class="centertext">Eliminar</td>
-                </tr>
-            </table>
-        </div>
-        <div class="col-12 mt-4">
-            <table class="table table-striped table-hover">
-                <tr>
-                    <th width="3%">Id</th>
-                    <th width="3%">Nome</th>
-                    <th colspan="3" class="centertext">Opções</th>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>Lisboa</td>
-                    <td width="5%" class="centertext">Editar</td>
-                    <td width="5%" class="centertext">Eliminar</td>
-                </tr>
-            </table>
-        </div>
-        <div class="col-12 mt-4">
-            <table class="table table-striped table-hover">
-                <tr>
-                    <th width="3%">Id</th>
-                    <th width="3%">Nome</th>
-                    <th colspan="3" class="centertext">Opções</th>
-                </tr>
-                <tr>
-                    <td>4</td>
-                    <td>Bragança</td>
-                    <td width="5%" class="centertext">Editar</td>
-                    <td width="5%" class="centertext">Eliminar</td>
-                </tr>
-            </table>
-        </div>
-    </div>
 </section>
+
+
 <!--<div class="container">
     <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1"
          aria-labelledby="staticBackdropLabel" aria-hidden="true">
@@ -108,47 +75,76 @@ toponovo();
             </div>
         </div>
     </div>
-</div> -->
-<div class="container">
+</div>-->
+<!--<div class="container"> -->
 
-    <!-- Modal Login-->
+<!-- Modal Login-->
 
-    <div class="modal fade" id="distrito" tabindex="-1" aria-labelledby="distrito" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <?php echo "<form action=\"confirmaNovoDistrito.php\" class=\"contact-form\" method=\"post\" enctype=\"multipart/form-data\">";
-                ?>
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="distrito">Adicionar Novo Distrito</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="row">
-                        <div class="modal-body">
-                            <div class="col-lg-6 mt-3 meio">
-                                <?php
-                                echo "<input type=\"text\" name=\"nomeDistrito\" placeholder='Nome do Distrito'><br>";
-                                ?>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="modal-footer mt-2">
-                        <div class="col-lg-7 meio">
-                            <button type="button" class="btn btn-dark mt-2" data-dismiss="modal">Fechar</button>
-                        </div>
-                        <div class="col-lg-5 meio">
-                            <button type="button" class="btn btn-primary mt-2">Adicionar</button>
+<div class="modal fade" id="distrito" tabindex="-1" aria-labelledby="distritolabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <?php echo "<form action=\"confirmaNovoDistrito.php\" class=\"contact-form\" method=\"post\" enctype=\"multipart/form-data\">";
+            ?>
+            <div class="modal-header">
+                <h5 class="modal-title" id="distritolabel">Adicionar Novo Distrito</h5>
 
-                        </div>
-                    </div>
-                <?php echo "</form >" ?>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
+            <div class="row">
+                <div class="modal-body">
+                    <div class="col-lg-6 mt-3 meio">
+                        <?php
+                        echo "<input type=\"text\" name=\"nomeDistrito\" placeholder='Nome do Distrito'><br>";
+                        ?>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer mt-2">
+                <div class="col-lg-7 meio">
+                    <button type="button" class="btn btn-dark mt-2" data-dismiss="modal">Fechar</button>
+                </div>
+                <div class="col-lg-5 meio">
+                    <button type="button" class="btn btn-primary mt-2">Adicionar</button>
+
+                </div>
+            </div>
+            <?php echo "</form >" ?>
         </div>
     </div>
 </div>
 
 
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdropDele" data-backdrop="static" data-keyboard="false" tabindex="-1"
+     aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <form action="eliminaDistrito.php" method="post" enctype="multipart/form-data">
+
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Eliminar Distrito</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+
+                    <div id="contentModalDelete"></div>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                    <button type="submit" class="btn btn-danger">Confirmar</button>
+                </div>
+        </div>
+        <input type="hidden" name="id" id="idDistrito">
+        </form >
+    </div>
+</div>
+
 <?php
-bottom();
+bot();
 ?>
