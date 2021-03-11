7ffaa18b36bdb4991b0c1b92f3391d18cdb6085b
Orlando Lopes <78795585+Kashigu@users.noreply.github.com>
2021-03-11 12:39:23 +0000

    tentar meter imagens nas categorias

--- AJAX/AJAXAddCategorias.php
+++ AJAX/AJAXAddCategorias.php
@@ -6,6 +6,8 @@ $categoria = addslashes($_POST['categoria']);
 $imagem=$_FILES['imagem']['name'];
 $novoNome="imagens/".$imagem;
 
+copy($_FILES['imagem']['tmp_name'],$novoNome);
+
 
 $sql="insert into categorias(categoriaId,categoriaNome,categoriaImagemURL) values(0,'".$categoria."','imagens/".$imagem."');";
 $result = mysqli_query($con, $sql);
