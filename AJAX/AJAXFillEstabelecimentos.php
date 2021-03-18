6bd33c84754db66ead688a20d44b6e507e220f47
Orlando Lopes <78795585+Kashigu@users.noreply.github.com>
2021-03-18 18:51:55 +0000

    estabelecimentos tabela com problemas

--- AJAX/AJAXFillEstabelecimentos.php
+++ AJAX/AJAXFillEstabelecimentos.php
@@ -3,7 +3,7 @@
 include_once("../includes/body.inc.php");
 $txt = addslashes($_POST['txt']);
 
-$sql = "Select * from estabelecimentos where estabelecimentoNome LIKE '%$txt%'";
+$sql = "Select * from estabelecimentos inner join categorias inner join perfis where estabelecimentoNome LIKE '%$txt%'";
 
 
 $result = mysqli_query($con, $sql);
@@ -15,7 +15,7 @@ $result = mysqli_query($con, $sql);
     <div class="container">
         <div class="col-12 mt-4">
             <table class="table table-striped table-hover">
-                <td colspan="6" align='right'>
+                <td colspan="7" align='right'>
                     <a href="../criarEstabelecimento.php"><i class='fas text-dark'> Adiciona</i></a>
                 </td>
                 <tr>
@@ -37,6 +37,7 @@ $result = mysqli_query($con, $sql);
                     <td><?php echo $dados['estabelecimentoNome'] ?></td>
                     <td><img width='100' height="100" src="../<?php echo $dados['estabelecimentoMiniaturaURL'] ?>"></td>
                     <td><?php echo $dados['perfilNome'] ?></td>
+                    <td><?php echo $dados['categoriaNome'] ?></td>
                     <td><a href="../novoperfil.php?id= <?php echo $dados['estabelecimentoId'];?>"> <i
                                     class="fas fa-edit text-primary"></i></a></td>
                     <td><a onclick="DeleteUtilizador(<?php echo $dados['estabelecimentoId'];?>)"> <i class="fas fa-trash  text-danger"></i></a></td>
