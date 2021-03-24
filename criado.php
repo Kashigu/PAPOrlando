5357f00c1059cb6093b418f0647fd674a9779c35
Orlando Lopes <78795585+Kashigu@users.noreply.github.com>
2021-03-24 17:52:53 +0000

    criado com comentários da tentativa de meter slideshow

--- criado.php
+++ criado.php
@@ -57,10 +57,49 @@ $dadosEstabelecimentos = mysqli_fetch_array($resultEstabelecimentos)
                             <h4>Sobre o Restaurante</h4>
                             <p> <?php echo $dadosEstabelecimentos['estabelecimentoDescricao']?> </p>
                         </div>
-                        <div class="about-video">
-                            <img class="tamanho1" src="<?php echo $dadosEstabelecimentos['estabelecimentoInteriorURL'] ?>" alt="">
+                        <div class="about-video1">
+                              <img class="tamanho1" src="<?php  echo $dadosEstabelecimentos['estabelecimentoInteriorURL'] ?>" alt="">
                             <a href="<?php echo $dadosEstabelecimentos['estabelecimentoInteriorURL'] ?>" class="img-hover pop-up"><img src="img/zoom.png" alt=""></a>
+                                <!--  <div id="carouselExampleIndicators" class="tamanho carousel slide"
+                                 data-ride="carousel">
+                                <ol class=" carousel-indicators">
+                                    <li data-target="#carouselExampleIndicators"
+                                        data-slide-to="1"></li>
+                                    <li data-target="#carouselExampleIndicators"
+                                        data-slide-to="2"></li>
+                                    <li data-target="#carouselExampleIndicators"
+                                        data-slide-to="3"></li>
 
+                                </ol>
+                                <div class="carousel-inner">
+                                    <?php/*
+                                    $sql="Select * from imagens";
+                                    $resultImagens=mysqli_query($con, $sql);
+                                    while ($dadosImagens = mysqli_fetch_array($resultImagens)) {
+                                    */?>
+                                    <div class="carousel-item active" <?php // echo $dadosImagens['imagemId'] ?>>
+                                        <img class=" tamanho" src="<?php //echo $dadosImagens['imagemURL'] ?>">
+                                        <a href="<?php //echo $dadosImagens['imagemURL'] ?>" class="img-hover pop-up"></a>
+
+                                    </div>
+                                    <?php/*
+                                    }
+                                    */?>
+
+
+                                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button"
+                                       data-slide="prev">
+                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                        <span class="sr-only">Previous</span>
+                                    </a>
+                                    <a class="carousel-control-next direita" href="#carouselExampleIndicators" role="button"
+                                       data-slide="next">
+                                        <span class="carousel-control-next-icon direita" aria-hidden="true"></span>
+                                        <span class="sr-only">Next</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div> -->
                         </div>
                         <div class="client-reviews">
                             <h3>Revisão</h3>
