5b9c728f88856bb8519117e4a6324b8843e471fa
Orlando Lopes <78795585+Kashigu@users.noreply.github.com>
2021-03-25 17:42:02 +0000

    TENTATIVA DE FILTRAGEM NO PROCURAR

--- AJAX/AJAXFillEstabelecimentosProcurar.php
+++ AJAX/AJAXFillEstabelecimentosProcurar.php
@@ -3,10 +3,36 @@ include_once("../includes/body.inc.php");
 top(PROCURAR);
 $txt=addslashes($_POST['txt']);
 
-$sql = "Select * from estabelecimentos where estabelecimentoNome LIKE '%$txt%'";
+$sql="
+                    select *
+                    from distritos inner join estabelecimentos 
+                    on distritoId=estabelecimentoDistritoId
+                    INNER JOIN categorias
+                    on categoriaId=estabelecimentoCategoriaId
+                    where estabelecimentoNome LIKE '%$txt%'";
 
-$result = mysqli_query($con, $sql);
+$resultEstabelecimentos = mysqli_query($con, $sql);
+while ($dadosEstabelecimentos = mysqli_fetch_array($resultEstabelecimentos)) {
+    ?>
+    <div class="col-lg-4 col-sm-6">
+        <a class="arrange-items"
+           href="single-listing.php?id=<?php echo $dadosEstabelecimentos['estabelecimentoId'] ?>">
+            <div class="arrange-pic">
+                <img class="centrinho"
+                     src="<?php echo $dadosEstabelecimentos['estabelecimentoMiniaturaURL'] ?>"
+                     alt="">
+                <div class="rating">4.9</div>
+                <div class="tic-text"><?php echo $dadosEstabelecimentos['categoriaNome'] ?></div>
+            </div>
+            <div class="arrange-text">
+                <h5><?php echo $dadosEstabelecimentos['estabelecimentoNome'] ?></h5>
+                <span><?php echo $dadosEstabelecimentos['estabelecimentoLocalidade'] ?></span>
+                <p><?php echo $dadosEstabelecimentos['estabelecimentoSlogan'] ?></p>
+                <div class="open tomorrow">Abre amanhã às 10 da manhã</div>
+            </div>
+        </a>
+    </div>
+    <?php
+}
 ?>
-
-
 
--- procurar.php
+++ procurar.php
@@ -59,10 +59,10 @@ top(PROCURAR);
                     <div class="location-search1 col-lg-10 mt-3 ">
                         <h5>Nome do Estabelecimento</h5>
                         <?php
-                        $txt=addslashes($_POST['txt']);
-                        $sql="Select * from estebelecimentos where estabelecimentoNome LIKE '%$txt%'";
+                        $txt = addslashes($_POST['txt']);
+                        $sql = "Select * from estebelecimentos where estabelecimentoNome LIKE '%$txt%'";
 
-                        $result=mysqli_query($con,$sql);
+                        $result = mysqli_query($con, $sql);
                         ?>
                         <input class="location-search1 nice-select1" type="text" id="search">
                     </div>
@@ -196,51 +196,21 @@ top(PROCURAR);
                                 <option>Mais Recente</option>
                             </select>
                         </form>
+
+                        <div id="tableContent"
+
                     </div>
-                    <?php
-                    $sql="
-                            select *
-                            from distritos inner join estabelecimentos 
-                            on distritoId=estabelecimentoDistritoId
-                            INNER JOIN categorias
-                            on categoriaId=estabelecimentoCategoriaId
-                            where estabelecimentoNome LIKE '%$txt%'";
-
-                    $resultEstabelecimentos = mysqli_query($con, $sql);
-                    while ($dadosEstabelecimentos = mysqli_fetch_array($resultEstabelecimentos)) {
-                        ?>
-                        <div class="col-lg-4 col-sm-6">
-                            <a class="arrange-items"
-                               href="single-listing.php?id=<?php echo $dadosEstabelecimentos['estabelecimentoId'] ?>">
-                                <div class="arrange-pic">
-                                    <img class="centrinho"
-                                         src="<?php echo $dadosEstabelecimentos['estabelecimentoMiniaturaURL'] ?>"
-                                         alt="">
-                                    <div class="rating">4.9</div>
-                                    <div class="tic-text"><?php echo $dadosEstabelecimentos['categoriaNome'] ?></div>
-                                </div>
-                                <div class="arrange-text">
-                                    <h5><?php echo $dadosEstabelecimentos['estabelecimentoNome'] ?></h5>
-                                    <span><?php echo $dadosEstabelecimentos['estabelecimentoLocalidade'] ?></span>
-                                    <p><?php echo $dadosEstabelecimentos['estabelecimentoSlogan'] ?></p>
-                                    <div class="open tomorrow">Abre amanhã às 10 da manhã</div>
-                                </div>
-                            </a>
-                        </div>
-                        <?php
-                    }
-                    ?>
-
-                    <div class="col-lg-12 text-right">
-                        <div class="pagination-num">
-                            <a href="#">01</a>
-                            <a href="#">02</a>
-                            <a href="#">03</a>
-                        </div>
+                </div>
+                <div class="col-lg-12 text-right">
+                    <div class="pagination-num">
+                        <a href="#">01</a>
+                        <a href="#">02</a>
+                        <a href="#">03</a>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     </div>
 </section>
 
