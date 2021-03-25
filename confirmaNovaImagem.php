62f8e086ed276e64d68c78dd600a9e67fd0a4d46
Orlando Lopes <78795585+Kashigu@users.noreply.github.com>
2021-03-25 19:34:21 +0000

    CRUD de adicionar imagem inicio

---
+++ confirmaNovaImagem.php
@@ -0,0 +1,23 @@
+<?php
+include_once ("includes/body.inc.php");
+$id=intval($_GET['id']);
+
+$categoria=$_POST['nomeImagem'];
+$imagem=$_FILES['imagem']['name'];
+$novoNome="imagens/".$imagem;
+
+copy($_FILES['imagem']['tmp_name'],"../".$novoNome);
+
+
+// A imagem só aparece porque está na pasta imagens/ mas se não tiver não aparece //
+
+
+
+//copy($_FILES['logoEmpresa']['tmp_name'],$novoNome);
+
+//$sql="insert into empresas(empresaNome,empresaLogoURL) values('".$nome."','imagens/".$imagem."');";
+
+$sql="insert into imagens(imagemId,imagemNome,imagemURL) values(0,'".$categoria."','".$novoNome."');";
+mysqli_query($con,$sql);
+header("location:galeria.php?id={$id}");
+?>
