362218306f5d5768743b2524a94c04280772ee84
Orlando Lopes <78795585+Kashigu@users.noreply.github.com>
2021-04-13 21:38:53 +0100

    confirma

--- criarEstabelecimento.php
+++ criarEstabelecimento.php
@@ -64,6 +64,64 @@ $id=intval($_GET['id']);
             contextmenu: "link image imagetools table",
         });
     </script>
+    <script>
+
+        tinymce.init({
+            selector: 'textarea#myTextarea1',
+            plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons',
+            imagetools_cors_hosts: ['picsum.photos'],
+            menubar: 'file edit view insert format tools table help',
+            toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+            toolbar_sticky: true,
+            autosave_ask_before_unload: true,
+            autosave_interval: "30s",
+            autosave_prefix: "{path}{query}-{id}-",
+            autosave_restore_when_empty: false,
+            autosave_retention: "2m",
+            image_advtab: true,
+            /*content_css: '//www.tiny.cloud/css/codepen.min.css',*/
+            link_list: [
+                { title: 'My page 1', value: 'https://www.codexworld.com' },
+                { title: 'My page 2', value: 'https://www.xwebtools.com' }
+            ],
+            image_list: [
+                { title: 'My page 1', value: 'https://www.codexworld.com' },
+                { title: 'My page 2', value: 'https://www.xwebtools.com' }
+            ],
+            image_class_list: [
+                { title: 'None', value: '' },
+                { title: 'Some class', value: 'class-name' }
+            ],
+            importcss_append: true,
+            file_picker_callback: function (callback, value, meta) {
+                /* Provide file and text for the link dialog */
+                if (meta.filetype === 'file') {
+                    callback('https://www.google.com/logos/google.jpg', { text: 'My text' });
+                }
+                /* Provide image and alt text for the image dialog */
+                if (meta.filetype === 'image') {
+                    callback('https://www.google.com/logos/google.jpg', { alt: 'My alt text' });
+                }
+                /* Provide alternative source and posted for the media dialog */
+                if (meta.filetype === 'media') {
+                    callback('movie.mp4', { source2: 'alt.ogg', poster: 'https://www.google.com/logos/google.jpg' });
+                }
+            },
+            templates: [
+                { title: 'New Table', description: 'creates a new table', content: '<div class="mceTmpl"><table width="98%%"  border="0" cellspacing="0" cellpadding="0"><tr><th scope="col"> </th><th scope="col"> </th></tr><tr><td> </td><td> </td></tr></table></div>' },
+                { title: 'Starting my story', description: 'A cure for writers block', content: 'Once upon a time...' },
+                { title: 'New list with dates', description: 'New List with dates', content: '<div class="mceTmpl"><span class="cdate">cdate</span><br /><span class="mdate">mdate</span><h2>My List</h2><ul><li></li><li></li></ul></div>' }
+            ],
+            template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
+            template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
+            height: 600,
+            image_caption: true,
+            quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+            noneditable_noneditable_class: "mceNonEditable",
+            toolbar_mode: 'sliding',
+            contextmenu: "link image imagetools table",
+        });
+    </script>
 
     <div class="hero-listing set-bg" data-setbg="img/maravilha.jpg">
     </div>
@@ -144,62 +202,16 @@ $id=intval($_GET['id']);
                         <div class="col-lg-12">
                             <textarea name="reviewTexto" id="myTextarea" placeholder="Sobre o Estabelecimento"></textarea>
                         </div>
-                        <div class="col-lg-6 mt-3">
+                        <div class="col-lg-6 mt-5">
                             <input type="text" name="rua" id="rua" placeholder="Rua">
                             <input type="tel" name="numero" id="numero" placeholder="Número">
                             <input type="email" name="email" id="email" placeholder="Email do Estabelecimento">
                         </div>
                         <div class="col-lg-6 mt-3">
                             <div class="row">
-                                <div class="col-5">
-                                    <label>Segunda a sexta</label>
-                                </div>
-                                <div class="col-7">
-                                    <input type="text" name="horas" id="horas" placeholder="8:00 - 22:00">
-                                </div>
-
+                                <label>Horas de Trabalho: </label>
+                                <textarea name="reviewTexto1" id="myTextarea1" placeholder="Horas de Trabalho"></textarea>
                             </div>
-                            <div class="row">
-                                <div class="col-5">
-                                    <label>Sábado</label>
-                                </div>
-                                <div class="col-7">
-                                    <input type="text" name="horas1" id="horas1" placeholder="8:00 - 22:00">
-                                </div>
-
-                            </div>
-                            <div class="row">
-                                <div class="col-5">
-                                    <label>Domingo</label>
-                                </div>
-                                <div class="col-7">
-                                    <input type="text" name="horas2" id="horas2" placeholder="8:00 - 22:00">
-                                </div>
-
-                            </div>
-                            <div class="row">
-                                <div class="col-5" name="descanso" id="descanso">
-                                    <label>Descanso Semanal</label>
-                                </div>
-                                <div class="col-5 arrange-select nice-select2">
-                                    <select>
-                                        <option>Sem descanso</option>
-                                        <option>Segunda-Feira</option>
-                                        <option>Terça-Feira</option>
-                                        <option>Quarta-Feira</option>
-                                        <option>Quinta-Feira</option>
-                                        <option>Sexta-Feira</option>
-                                        <option>Sábado</option>
-                                        <option>Domingo</option>
-                                    </select>
-                                </div>
-
-                            </div>
-
-
-                            <hr>
-
-
                         </div>
                         <div class="col-lg-6">
                             <label>Logótipo: </label>
