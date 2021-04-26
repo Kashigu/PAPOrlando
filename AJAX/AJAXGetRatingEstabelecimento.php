6e239f33ebed7aef441c90df9e0b9ecad2f9947b
Kashigu <orlandojslopes@gmail.com>
2021-04-26 11:36:30 +0100

    faz insert nas redes mas tem erros

---
+++ AJAX/AJAXGetRatingEstabelecimento.php
@@ -0,0 +1,38 @@
+<?php
+// dados na base de dados
+include_once("../includes/body.inc.php");
+session_start();
+$id=intval($_POST['idRede']);
+$valor=intval($_POST['valor']);
+$sql="Select count(*) as n
+        from redes 
+        where redeEstabelecimentoId=$id and redeTipo='rating'";
+
+$result = mysqli_query($con, $sql);
+$dados = mysqli_fetch_array($result);
+
+// verifica se o utilizador gosta da foto
+
+$sqlRating = "insert into ratings (ratingRedeId, ratingValor) values('".$id. "','" . $valor."')";
+mysqli_query($con,$sql);
+
+
+
+
+$sql="select * from redes where redePerfilId=".$_SESSION['id']." and redeEstabelecimentoId=".$id." and redeTipo='rating'";
+mysqli_query($con,$sql);
+if(mysqli_affected_rows($con)>0){
+    ?>
+    <i class="fa fa-thumbs-up" style="color:yellow" aria-hidden="true"></i>
+    <?php
+}else{
+    ?>
+    <i class="fa fa-thumbs-up" aria-hidden="true"></i>
+    <?php
+}
+?>
+
+</span>
+ <?php
+
+?>
--- AJAX/AJAXPutRatingEstabelecimento.php
+++ AJAX/AJAXPutRatingEstabelecimento.php
@@ -6,8 +6,24 @@ session_start();
 $valor=intval($_POST['valor']);
 $idRede=intval($_POST['idRede']);
 
- echo $sql = "insert into ratings (ratingRedeId, ratingValor) values('".$idRede. "','" . $valor."')";
+ $sql = "insert into ratings (ratingRedeId, ratingValor) values('".$idRede. "','" . $valor."')";
  mysqli_query($con,$sql);
+
+
+$sql="select * from redes where redePerfilId=".$_SESSION['id']." and redeEstabelecimentoId=". $idRede . " and redeTipo='rating'";
+mysqli_query($con,$sql);
+
+
+ $sql = "insert into redes (redeId, redePerfilId, redeEstabelecimentoId, redeTipo) values(0,".$_SESSION['id']. "," . $idRede.",'rating')";
+
+mysqli_query($con,$sql);
+
+bot();
+?>
+
+
+
+
 
  // insert into redes (redeId, redeEstabelecimentoId, redePerfilId, redeTipo) VALUES (0,'1','2','rating')
 
