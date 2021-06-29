2d1890212bef8975f0ee3338102e7173a461ac00
Kashigu <orlandojslopes@gmail.com>
2021-06-29 11:12:27 +0100

    width

--- AJAX/AJAXFillReservas.php
+++ AJAX/AJAXFillReservas.php
@@ -5,7 +5,7 @@ $txt = addslashes($_POST['txt']);
 $id = intval($_POST['id']);
 $sql = "Select * from reservas inner join estabelecimentos on reservaEstabelecimentoId=estabelecimentoId
                                inner join perfis on reservaPerfilId=perfilId
-                               where perfilId=".$id." and estabelecimentoNome LIKE '%$txt%'";
+                               where perfilId=" . $id . " and estabelecimentoNome LIKE '%$txt%'";
 
 $resultEstabelecimentos = mysqli_query($con, $sql);
 
@@ -19,21 +19,37 @@ while ($dadosEstabelecimentos = mysqli_fetch_array($resultEstabelecimentos)) {
 
             <div class="col-3 mt-2">
 
-                <a href="single-listing.php?id=<?php echo $dadosEstabelecimentos['estabelecimentoId'] ?>"><img class="quadradinho" src="<?php echo $dadosEstabelecimentos['estabelecimentoMiniaturaURL'] ?>"></a>
+                <a href="single-listing.php?id=<?php echo $dadosEstabelecimentos['estabelecimentoId'] ?>"><img
+                            class="quadradinho"
+                            src="<?php echo $dadosEstabelecimentos['estabelecimentoMiniaturaURL'] ?>"></a>
             </div>
             <div class="col-9 mt-2">
                 <table class="table table-striped table-hover">
                     <tr>
                         <th width="35%">Data</th>
-                        <th >Descrição</th>
+                        <th>Descrição</th>
                         <th colspan="2" class="centertext">Opções</th>
                     </tr>
                     <tr>
+                        <?php if ($dadosEstabelecimentos['reservaEstado'] == 'aceitar') { ?>
+
+                            <td><?php echo $dadosEstabelecimentos['reservaData'] ?></td>
+                            <td><?php echo $dadosEstabelecimentos['reservaDescricao'] ?></td>
+                            <td width="30%" class="centertext">Reserva Aceite</td>
+                            <?php
+                        }else{
+                        ?>
                         <td><?php echo $dadosEstabelecimentos['reservaData'] ?></td>
                         <td><?php echo $dadosEstabelecimentos['reservaDescricao'] ?></td>
-                        <td class="centertext"><a onclick="editaReserva(<?php echo $dadosEstabelecimentos['reservaId'];?>)" ><i
+                        <td class="centertext"><a
+                                    onclick="editaReserva(<?php echo $dadosEstabelecimentos['reservaId']; ?>)"><i
                                         class="fa fa-edit text-primary"></i></a></td>
-                        <td class="centertext"><a onclick="eliminaReserva(<?php echo $dadosEstabelecimentos['reservaId'];?>)"> <i class="fa fa-trash  text-danger"></i></a></td>
+                        <td class="centertext"><a
+                                    onclick="eliminaReserva(<?php echo $dadosEstabelecimentos['reservaId']; ?>)"> <i
+                                        class="fa fa-trash  text-danger"></i></a></td>
+                        <?php
+                        }
+                        ?>
                     </tr>
                 </table>
             </div>
